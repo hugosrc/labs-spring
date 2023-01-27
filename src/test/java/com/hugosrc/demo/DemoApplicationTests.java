39fb02cc9331dfb717027d9ebcfb85596480c4a7
hugosrc <hugosr.contato@gmail.com>
2023-01-27 07:30:56 -0300

    initial commit

---
+++ src/test/java/com/hugosrc/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugosrc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
